cf3b61b272998a0a99e294ba9bf25f5f250f81e9
tuhlopuz1 <tuhlopuz1@gmail.com>
2025-03-01 12:50:13 +0300

    Сделал распознавание фильма по фото

---
+++ backend/fastapi_server/screenshots/__init__.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from .get_movie_by_screenshot import router as get_movie_by_screenshot_router
+
+
+
+router = APIRouter()
+
+
+router.include_router(get_movie_by_screenshot_router)
