c0eca1dd52d55975eafe5d3aedd4b4e7aca6434d
jily64 <dimondima2008gun@gmail.com>
2025-03-01 11:40:37 +0300

    Moved TMDB Models to own file

--- backend/models/schemas.py
+++ backend/models/schemas.py
@@ -11,37 +11,3 @@ class Register(BaseModel):
     email: str
     password: str
     login: str
-
-
-class TMDBobject_TV(BaseModel):
-    adult:bool = True
-    backdrop_path:str
-    genre_ids:list[int] = []
-    id: int = 0
-    name:str
-    original_name:str
-    original_language:str
-    overview:str
-    popularity:float = 0.0
-    poster_path:str
-    first_air_date:str
-    video:bool = True
-    vote_average:float = 0.0
-    vote_count:int = 0.0
-
-class TMDBobject_Movie(BaseModel):
-    adult:bool = True
-    backdrop_path:str
-    genre_ids:list[int] = []
-    id: int = 0
-    title:str
-    original_title:str
-    original_language:str
-    overview:str
-    popularity:float = 0.0
-    poster_path:str
-    release_date:str
-    video:bool = True
-    vote_average:float = 0.0
-    vote_count:int = 0.0
-
