12d0a82b530cb0d68997c3bef991711a80578756
jily64 <dimondima2008gun@gmail.com>
2025-03-01 17:10:39 +0300

    New requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,8 @@ from fastapi_server.auth import router as authorization_router
 from fastapi_server.images import router as images_router
 from fastapi_server.screenshots import router as screenshots_router
 import os
+
+from adapters import db_source
 app = FastAPI()
 
 app.add_middleware(
@@ -19,13 +21,16 @@ app.include_router(authorization_router, prefix="/auth", tags=["Auth"])
 app.include_router(images_router, prefix="/images", tags=["Images"])
 app.include_router(screenshots_router, prefix="/screenshots", tags=["Screenshots"])
 
-
-@app.get("/api/user/tmdb/")
+@app.get("/api/user/tmdb_source/")
 async def get_short_film():
-    return "Penis valeriy"
+    return "No Redirections"
+
+@app.get("/api/user/collection")
+async def collection():
+
+    return
 
 
 if __name__ == "__main__":
-    
     host, port = 'localhost', '8000'
     uvicorn.run(app, host=host, port=int(port))
