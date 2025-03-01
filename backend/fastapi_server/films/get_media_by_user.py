fdf4195e94400d5c72c518ac4df7a490912d6d0f
TimofeyYA1 <timdim2018@mail.ru>
2025-03-01 16:55:03 +0300

    ee

---
+++ backend/fastapi_server/films/get_media_by_user.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, HTTPException, status,Security
+from models.schemas import Token, Film
+from adapters.db_source import DatabaseAdapter
+from fastapi.security import HTTPBearer
+from utils.functions import get_user
+router = APIRouter()
+Bear = HTTPBearer(auto_error=False)
+
+@router.get("/get_films", status_code=status.HTTP_200_OK)
+async def add_media(token:str = Security(Bear)):
+    user = get_user(token.credentials)
+    if user == []:
+        raise HTTPException(status_code=404, detail="Invalid credentials")
+    user = user[0]
+    adapter = DatabaseAdapter()
+    adapter.connect()
+    adapter.initialize_tables()
+    email_check = adapter.get_by_value('users', 'email', user["email"])
+    if len(email_check) == 0:
+        raise HTTPException(status_code=404, detail="User with this email does not exists")
+    films = adapter.get_by_value('films_to_users', 'email', user["email"])
+    print(films)
+    for i in range(len(films)):
+        films[i] = films[i]["media_id"]  
+    #возвращает список айдишников фильмов
+    return films
---
+++ backend/fastapi_server/films/search.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, HTTPException, status,Security
+from models.schemas import Token, Film
+from adapters.db_source import DatabaseAdapter
+from fastapi.security import HTTPBearer
+from utils.functions import get_user
+from tmdb_main import search_multi
+router = APIRouter()
+Bear = HTTPBearer(auto_error=False)
+
+@router.get("/search_film", status_code=status.HTTP_200_OK)
+async def add_media(search: str,token:str = Security(Bear)):
+    media = search_multi(search)
+    return media
