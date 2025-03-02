babf8c92a8e3da9139428421ec6170735f0ddec7
TimofeyYA1 <timdim2018@mail.ru>
2025-03-02 12:19:03 +0300

    change login added

---
+++ backend/fastapi_server/auth/change_login.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException, status,Security
+from models.schemas import Token, Film
+from adapters.db_source import DatabaseAdapter
+from fastapi.security import HTTPBearer
+from utils.functions import get_user
+from models.TMDB import TMDBobject_Short
+
+router = APIRouter()
+Bear = HTTPBearer(auto_error=False)
+
+@router.put("/change_login", status_code=200)
+async def mark_as_watched(login:str, token:str = Security(Bear)):
+    user = get_user(token.credentials)
+    if user == []:
+        raise HTTPException(status_code=501, detail="Invalid credentials")
+    user = user[0]
+    adapter = DatabaseAdapter()
+    adapter.connect()
+    adapter.initialize_tables()
+    user["login"] = login
+    adapter.execute_with_request(f"UPDATE users SET login = {login} WHERE email = {user['email']}")
+    return {"success": True}
