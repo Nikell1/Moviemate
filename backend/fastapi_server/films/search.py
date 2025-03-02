ff2f1d5afd390c434d4b87547be4fda15826b288
jily64 <dimondima2008gun@gmail.com>
2025-03-02 23:24:35 +0300

    Fixed Filters

--- backend/fastapi_server/films/search.py
+++ backend/fastapi_server/films/search.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, HTTPException, status,Security
 from fastapi.security import HTTPBearer
+
+from models.schemas import Search
 from utils.functions import get_user
 from adapters.tmdb import search_multi
 router = APIRouter()
@@ -7,11 +9,11 @@ Bear = HTTPBearer(auto_error=False)
 
 
 @router.get("/search_film", status_code=status.HTTP_200_OK)
-async def search_film(search: str, token:str = Security(Bear)):
+async def search_film(search: str, body: Search, token:str = Security(Bear)):
     user = get_user(token.credentials)
     print(token.credentials)
     if user == []:
         raise HTTPException(status_code=401, detail="Invalid token")
     user = user[0]
-    media = await search_multi(search,limit=19)
+    media = await search_multi(search,limit=19,release_date_high=body.release_date_high, release_date_low=body.release_date_low, genre_ids=body.genre_ids)
     return media
