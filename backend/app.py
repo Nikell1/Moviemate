34dff4af7716eb0dd544385c3e46354f8ee6ec74
TimofeyYA1 <timdim2018@mail.ru>
2025-03-02 14:21:34 +0300

    fixxx

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from fastapi_server.auth import router as authorization_router
 from fastapi_server.screenshots import router as screenshots_router
 from fastapi_server.films import router as films_router
 from fastapi_server.social import router as social_router
+from fastapi_server.collections import  router as collection_router
 import os
 load_dotenv()
 app = FastAPI(docs_url="/api/docs")
@@ -14,14 +15,14 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Разрешить все источники
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"],    
     allow_headers=["*"],
 )
 
 app.include_router(authorization_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(screenshots_router, prefix="/api/screenshots", tags=["Screenshots"])
 app.include_router(films_router, prefix="/api/films", tags=["Films"])
-app.include_router(social_router, prefix="/api/title", tags=["Title"])
+app.include_router(social_router, prefix="/api/collections", tags=["Collections"])
 app.include_router(social_router, prefix="/api/social", tags=["Social"])
 if __name__ == "__main__":
     host, port = os.getenv("FAST_API_HOST"), os.getenv("FAST_API_PORT")
