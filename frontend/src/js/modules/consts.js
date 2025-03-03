830846cb248b09925766fcd9d03b368448b59eaa
tuhlopuz1 <tuhlopuz1@gmail.com>
2025-03-03 18:06:14 +0300

    Кэш

--- frontend/src/js/modules/consts.js
+++ frontend/src/js/modules/consts.js
@@ -10,4 +10,4 @@ export const searchHash = 'search'
 
 export const accentColor = '#003BBE'
 
-export const BACKEND_URL='https://prod-team-24-jhaklm26.final.prodcontest.ru'
+export const BACKEND_URL='http://localhost:8000'
